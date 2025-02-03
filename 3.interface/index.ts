9deb0eb647120bc1e572d3c261f07202c262f577
AnsaAli <chinjuansa@example.com>
2025-02-03 12:59:40 +0530

    add interface

--- 3.interface/index.ts
+++ 3.interface/index.ts
@@ -1,12 +1,25 @@
 interface Details {
     name : string;
     age : number;
-    salary : number
+    salary : number;
+    getName : ()=> void
     
 }
 
 let userDetails : Details={
     age: 20,
     name : 'Ansa',
-    salary : 20000
+    salary : 20000,
+    getName(){
+        console.log(this.name)
+    }
+}
+
+let adminDetails : Details={
+    age : 23,
+    name : "Admin",
+    salary : 30000,
+    getName() {
+        console.log(this.age)
+    },
 }
