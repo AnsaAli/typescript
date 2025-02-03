49f927a37d143925482f6f39a5d660661cc8627d
AnsaAli <chinjuansa@example.com>
2025-02-03 11:05:15 +0530

    infer types/ implicit types

---
+++ 1.infer types/index.ts
@@ -0,0 +1,6 @@
+//infer types / implicit types
+
+let username = 'Ansa';
+//above username is a string, and if we try to assign
+//username = 10 , will show error. ie, ts will automatically detect the types
+// and then, it will show/ infer the error
