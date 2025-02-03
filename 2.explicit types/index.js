15443f68531ea000ca9d8f4fd8f5d987b935ed9f
AnsaAli <chinjuansa@example.com>
2025-02-03 11:18:54 +0530

    add explicit types

---
+++ 2.explicit types/index.js
@@ -0,0 +1,16 @@
+//defining types / explicit types
+//here we are defining the type
+var username = 'Ansa Ali';
+var zipcode = 102030;
+var married = true;
+//to show array of string, each elemts should be string if we specify string[]
+var skills = ["JS", "TS"];
+//array of numbers
+var numbers = [10, 20, 30];
+//if it is [], then ts expet it to be an empty array
+var emptyarray = [];
+//define object
+var user = {
+    age: 20,
+    name: 'Ansa'
+};
---
+++ 2.explicit types/index.ts
@@ -0,0 +1,20 @@
+//defining types / explicit types
+//here we are defining the type
+
+let username : string = 'Ansa Ali';
+let zipcode : number = 10_20_30;
+let married : boolean = true;
+
+//to show array of string, each elemts should be string if we specify string[]
+let skills : string[] = ["JS","TS"]
+//array of numbers
+let numbers: number[]=[10,20,30]
+//if it is [], then ts expet it to be an empty array
+let emptyarray:[] =[]
+
+//define object
+let user :{name : string, age : number}={
+    age : 20,
+    name : 'Ansa'
+}
+
